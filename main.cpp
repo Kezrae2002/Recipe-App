d560a155673c015c22f2ae2415ccae17ebd18bfd
DarkWolf2804 <aramsey89@ivytech.edu>
2026-02-18 19:04:20 +0000

    Check main

--- main.cpp
+++ main.cpp
@@ -1,12 +1,3 @@
 /*
-We need some class ideas for the program if you have any put them here
-Auston's ideas
-Recipe
-Ingredient
-RecipeBook
-
-Kezrae Ideas
-SearchEngine(searchByName(), searchByIngredient(), searchByCategory())
-
-SQLite added will check agian at a later date to confirm
+Get a base line for the class functions so we can start working on connecting everything in the main as soon as possible
 */
